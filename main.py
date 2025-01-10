1831aa5144bf920a204fcc662b0ccd58f7a09227
Marcelo Portella <marcelo.portella@outlook.com>
2025-01-10 16:48:47 -0300

    exemplos de print e input

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("first commit with feature")
+# nome = input("Digite seu nome: ")
+# qtde = len(nome)
+
+# print(f"O nome é {nome} e a quantidade de caratereces é {qtde}")
+
+# # Ou
+
+# print (len(input("Nome: ")))
+
+valor = int(input("Valor 1: "))
+valor2 = int(input("Valor 2: "))
+
+print(f"A soma é {valor + valor2}")
